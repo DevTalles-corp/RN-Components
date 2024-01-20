2ace2e02fef896b25f8c69984d83a28987ff55f9
Fernando Herrera <fernando.herrera85@gmail.com>
2024-01-20 10:44:07 -0500

    Delete App.tsx and register ComponentsApp in index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
 import {name as appName} from './app.json';
+import { ComponentsApp } from './src/ComponentsApp';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => ComponentsApp);
---
+++ src/ComponentsApp.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native';
+
+export const ComponentsApp = () => {
+  return (
+    <View>
+      <Text>ComponentsApp</Text>
+      
+    </View>
+  )
+}
