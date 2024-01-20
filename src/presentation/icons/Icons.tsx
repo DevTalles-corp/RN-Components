31037cc97ec66960336433e50e5f7eb852b6d114
Fernando Herrera <fernando.herrera85@gmail.com>
2024-01-20 10:52:07 -0500

    Add AirplaneIcon to HomeScreen

---
+++ src/presentation/icons/Icons.tsx
@@ -0,0 +1,5 @@
+import Icon from 'react-native-vector-icons/Ionicons';
+
+
+
+export const AirplaneIcon = () => <Icon name="airplane-outline" size={30} />;
--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,9 +1,12 @@
 import { Text, View } from 'react-native';
+import { AirplaneIcon } from '../../icons/Icons';
 
 export const HomeScreen = () => {
   return (
     <View>
       <Text>HomeScreen</Text>
+
+      <AirplaneIcon />
       
     </View>
   )
