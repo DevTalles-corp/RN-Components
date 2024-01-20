ac47cb405c4df676f704c3c7a6192d1005c711cf
Fernando Herrera <fernando.herrera85@gmail.com>
2024-01-20 12:10:12 -0500

    Add menu items and update UI in HomeScreen

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,13 +1,115 @@
-import { Text, View } from 'react-native';
-import { AirplaneIcon } from '../../icons/Icons';
+import {Text, View} from 'react-native';
+import {globalStyles} from '../../../config/theme/theme';
+import {ScrollView} from 'react-native-gesture-handler';
+import {Title} from '../../components/ui/Title';
+import {MenuItem} from '../../components/ui/MenuItem';
+
+const animationMenuItems = [
+  {
+    name: 'Animation 101',
+    icon: 'cube-outline',
+    component: 'Animation101Screen',
+  },
+  {
+    name: 'Animation 102',
+    icon: 'albums-outline',
+    component: 'Animation102Screen',
+  },
+];
+
+export const menuItems = [
+  {
+    name: 'Pull to refresh',
+    icon: 'refresh-outline',
+    component: 'PullToRefreshScreen',
+  },
+  {
+    name: 'Section List',
+    icon: 'list-outline',
+    component: 'CustomSectionListScreen',
+  },
+  {
+    name: 'Modal',
+    icon: 'copy-outline',
+    component: 'ModalScreen',
+  },
+  {
+    name: 'InfiniteScroll',
+    icon: 'download-outline',
+    component: 'InfiniteScrollScreen',
+  },
+  {
+    name: 'Slides',
+    icon: 'flower-outline',
+    component: 'SlidesScreen',
+  },
+  {
+    name: 'Themes',
+    icon: 'flask-outline',
+    component: 'ChangeThemeScreen',
+  },
+];
+const uiMenuItems = [
+  {
+    name: 'Switches',
+    icon: 'toggle-outline',
+    component: 'SwitchScreen',
+  },
+  {
+    name: 'Alerts',
+    icon: 'alert-circle-outline',
+    component: 'AlertScreen',
+  },
+  {
+    name: 'TextInputs',
+    icon: 'document-text-outline',
+    component: 'TextInputScreen',
+  },
+];
 
 export const HomeScreen = () => {
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View style={[globalStyles.mainContainer]}>
+      <View style={globalStyles.globalMargin}>
+        <ScrollView>
+          <Title text="Opciones del menú" safe />
 
-      <AirplaneIcon />
-      
+          {/* animationMenuItems
+          menuItems
+          uiMenuItems */}
+
+          {animationMenuItems.map((item, index) => (
+            <MenuItem
+              key={item.component}
+              {...item}
+              isFirst={index === 0}
+              isLast={index === animationMenuItems.length - 1}
+            />
+          ))}
+
+          <View style={{marginTop: 30}} />
+          {menuItems.map((item, index) => (
+            <MenuItem
+              key={item.component}
+              {...item}
+              isFirst={index === 0}
+              isLast={index === menuItems.length - 1}
+            />
+          ))}
+
+          <View style={{marginTop: 30}} />
+          {uiMenuItems.map((item, index) => (
+            <MenuItem
+              key={item.component}
+              {...item}
+              isFirst={index === 0}
+              isLast={index === uiMenuItems.length - 1}
+            />
+          ))}
+
+          <View style={{marginTop: 30}} />
+        </ScrollView>
+      </View>
     </View>
-  )
-}
+  );
+};
