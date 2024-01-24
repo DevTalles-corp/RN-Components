fbaf12a6f5cc3d0a9516432a62887b52c2f3d7ca
Fernando Herrera <fernando.herrera85@gmail.com>
2024-01-24 10:11:08 -0500

    Add Animation102Screen to Navigator

--- src/presentation/navigator/Navigator.tsx
+++ src/presentation/navigator/Navigator.tsx
@@ -1,6 +1,7 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import { HomeScreen } from '../screens/home/HomeScreen';
 import { Animation101Screen } from '../screens/animations/Animation101Screen';
+import { Animation102Screen } from '../screens/animations/Animation102Screen';
 
 const Stack = createStackNavigator();
 
@@ -11,6 +12,7 @@ export const Navigator = () => {
     }}>
       <Stack.Screen name="HomeScreen" component={HomeScreen} />
       <Stack.Screen name="Animation101Screen" component={Animation101Screen} />
+      <Stack.Screen name="Animation102Screen" component={Animation102Screen} />
       
     </Stack.Navigator>
   );
---
+++ src/presentation/screens/animations/Animation102Screen.tsx
@@ -0,0 +1,53 @@
+import { useRef } from 'react';
+import { Animated, PanResponder, StyleSheet, View } from 'react-native';
+
+export const Animation102Screen = () => {
+
+  const pan = useRef(new Animated.ValueXY()).current;
+
+  const panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderMove: Animated.event([
+      null,
+      {
+        dx: pan.x, // x,y are Animated.Value
+        dy: pan.y,
+      },
+    ], {
+      useNativeDriver: false
+    }),
+
+    onPanResponderRelease: () => {
+      Animated.spring(
+        pan, // Auto-multiplexed
+        {toValue: {x: 0, y: 0}, useNativeDriver: false }, // Back to zero
+      ).start();
+    },
+  });
+
+
+  return (
+    <View style={styles.container}>
+      <Animated.View
+        {...panResponder.panHandlers}
+        style={[pan.getLayout(), styles.box]}
+      />
+    </View>
+  )
+}
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  box: {
+    backgroundColor: '#61dafb',
+    width: 80,
+    height: 80,
+    borderRadius: 4,
+  },
+});
