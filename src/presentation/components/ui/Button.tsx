97bb336db46b029d28196039e19eaa72e848118e
Fernando Herrera <fernando.herrera85@gmail.com>
2024-01-24 13:17:12 -0500

    Add Button component

---
+++ src/presentation/components/ui/Button.tsx
@@ -0,0 +1,35 @@
+import { Pressable, StyleProp, Text, View, ViewStyle } from 'react-native';
+import { colors, globalStyles } from '../../../config/theme/theme';
+
+
+interface Props {
+  text: string;
+  styles?: StyleProp<ViewStyle>;
+
+  onPress: () => void;
+}
+
+
+
+export const Button = ({ text, styles, onPress }:Props) => {
+  return (
+    <Pressable
+      onPress={ onPress }
+      style={ ({ pressed }) => ([
+        globalStyles.btnPrimary,
+        {
+          opacity: pressed ? 0.8 : 1,
+          backgroundColor: colors.primary,
+        }
+      ]) }
+    >
+      <Text style={[
+        globalStyles.btnPrimaryText,
+        {
+          color: colors.buttonTextColor
+        }
+      ]}>Button</Text>
+      
+    </Pressable>
+  )
+}
