765305e97ae299b6bfbcebcd370a074bc5472e60
Fernando Herrera <fernando.herrera85@gmail.com>
2024-01-24 09:53:07 -0500

    Refactor animation code in Animation101Screen.tsx

---
+++ src/presentation/hooks/useAnimation.tsx
@@ -0,0 +1,64 @@
+import {useRef} from 'react';
+import {Animated, Easing} from 'react-native';
+
+export const useAnimation = () => {
+  const animatedOpacity = useRef(new Animated.Value(0)).current;
+  const animatedTop = useRef(new Animated.Value(0)).current;
+
+  const fadeIn = ({duration = 300, toValue = 1, callback = () => {}}) => {
+    // Animated.timing( animatedTop, {
+    //   toValue: 0,
+    //   duration: 700,
+    //   useNativeDriver: true,
+    //   easing: Easing.bounce, //Easing.elastic(2)
+    // }).start( () => console.log('Animation ended') );
+
+    Animated.timing(animatedOpacity, {
+      toValue: toValue,
+      duration: duration,
+      useNativeDriver: true,
+    }).start(callback);
+  };
+
+  const fadeOut = ({duration = 300, toValue = 0, callback = () => {}}) => {
+    Animated.timing(animatedOpacity, {
+      toValue: toValue,
+      duration: duration,
+      useNativeDriver: true,
+    }).start(callback);
+
+    //() => animatedTop.resetAnimation()
+  };
+
+  const startMovingTopPosition = ({
+    initialPosition = 0,
+    toValue = 0,
+    duration = 300,
+    easing = Easing.linear,
+    callback = () => {}
+  }) => {
+
+    animatedTop.setValue( initialPosition );
+    Animated.timing( animatedTop, {
+      toValue: toValue,
+      duration: duration,
+      useNativeDriver: true,
+      easing: easing
+    }).start( callback );
+
+
+  }
+
+
+
+  return {
+    // Properties
+    animatedOpacity,
+    animatedTop,
+
+    // Methods
+    fadeIn,
+    fadeOut,
+    startMovingTopPosition,
+  };
+};
--- src/presentation/screens/animations/Animation101Screen.tsx
+++ src/presentation/screens/animations/Animation101Screen.tsx
@@ -1,42 +1,12 @@
 import { Animated, Easing, Pressable, StyleSheet, Text, View } from 'react-native';
 import { colors } from '../../../config/theme/theme';
-import { useRef } from 'react';
+
+import { useAnimation } from '../../hooks/useAnimation';
 
 
 export const Animation101Screen = () => {
 
-  
-  const animatedOpacity = useRef( new Animated.Value(0) ).current;
-  const animatedTop = useRef( new Animated.Value(-100) ).current;
-  
-  const fadeIn = () => {
-
-    Animated.timing( animatedTop, {
-      toValue: 0,
-      duration: 700,
-      useNativeDriver: true,
-      easing: Easing.bounce, //Easing.elastic(2)
-    }).start( () => console.log('Animation ended') );
-
-
-
-    Animated.timing( animatedOpacity, {
-      toValue: 1,
-      duration: 300,
-      useNativeDriver: true,
-    }).start( () => console.log('Animation ended') );
-  }
-
-  const fadeOut = () => {
-    Animated.timing( animatedOpacity, {
-      toValue: 0,
-      duration: 300,
-      useNativeDriver: true,
-    }).start( () => animatedTop.resetAnimation() );
-
-    
-
-  }
+  const { fadeIn, fadeOut, animatedOpacity, animatedTop, startMovingTopPosition } = useAnimation();
 
 
   return (
@@ -52,11 +22,19 @@ export const Animation101Screen = () => {
         }
       ]}/>
 
-      <Pressable onPress={ fadeIn } style={{ marginTop: 10 }}>
+      <Pressable onPress={ () => {
+        fadeIn({});
+        startMovingTopPosition({ 
+          initialPosition: -100, 
+          easing: Easing.elastic(1),
+          duration: 750
+        });
+
+      }} style={{ marginTop: 10 }}>
         <Text>FadeIn</Text>
       </Pressable>
 
-      <Pressable onPress={ fadeOut } style={{ marginTop: 10 }}>
+      <Pressable onPress={ ()=> fadeOut({}) } style={{ marginTop: 10 }}>
         <Text>FadeOut</Text>
       </Pressable>
       
