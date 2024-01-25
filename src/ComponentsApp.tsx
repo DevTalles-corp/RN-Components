6080e6d82f37aa5be9e01cb2d0c0b6d154c8f37c
Fernando Herrera <fernando.herrera85@gmail.com>
2024-01-25 11:43:28 -0500

    Refactor navigation and theming in ComponentsApp

--- src/ComponentsApp.tsx
+++ src/ComponentsApp.tsx
@@ -1,23 +1,30 @@
 import 'react-native-gesture-handler';
 
-import { NavigationContainer} from '@react-navigation/native';
+import {DarkTheme, DefaultTheme, NavigationContainer} from '@react-navigation/native';
 import {Navigator} from './presentation/navigator/Navigator';
-import {PropsWithChildren} from 'react';
-import {ThemeProvider} from './presentation/context/ThemeContext';
+import {PropsWithChildren, useContext} from 'react';
+import {ThemeContext, ThemeProvider} from './presentation/context/ThemeContext';
 
-
-const AppState = ({children}: PropsWithChildren) => {
+const AppNavigation = ({children}: PropsWithChildren) => {
+  const { isDark } = useContext(ThemeContext);
   return (
-    <NavigationContainer>
-      <ThemeProvider>{children}</ThemeProvider>
-    </NavigationContainer>
+    <NavigationContainer theme={ isDark ? DarkTheme : DefaultTheme }>{children}</NavigationContainer>
+  );
+};
+
+
+const AppTheme = ({children}: PropsWithChildren) => {
+  return (
+    <ThemeProvider>
+      <AppNavigation>{children}</AppNavigation>
+    </ThemeProvider>
   );
 };
 
 export const ComponentsApp = () => {
   return (
-    <AppState>
+    <AppTheme>
       <Navigator />
-    </AppState>
+    </AppTheme>
   );
 };
