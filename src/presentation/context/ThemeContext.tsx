a8a609efd54afd8a1043e43b0181c808255d3fe5
Fernando Herrera <fernando.herrera85@gmail.com>
2024-01-25 12:04:34 -0500

    Add useColorScheme hook to ThemeContext

--- src/presentation/context/ThemeContext.tsx
+++ src/presentation/context/ThemeContext.tsx
@@ -1,5 +1,6 @@
-import { PropsWithChildren, createContext, useState } from 'react';
+import { PropsWithChildren, createContext, useEffect, useState } from 'react';
 import { ThemeColors, darkColors, lightColors } from '../../config/theme/theme';
+import { AppState, Appearance, useColorScheme } from 'react-native';
 
 type ThemeColor = 'light' | 'dark';
 
@@ -18,7 +19,32 @@ export const ThemeContext = createContext({} as ThemeContextProps );
 
 export const ThemeProvider = ({ children }: PropsWithChildren ) => {
 
+  const colorScheme = useColorScheme();
   const [currentTheme, setCurrentTheme] = useState<ThemeColor>('light');
+
+
+  useEffect(() => {
+    if ( colorScheme === 'dark' ) {
+      setCurrentTheme('dark');
+    } else {
+      setCurrentTheme('light');
+    }
+  }, [colorScheme]);
+
+  // useEffect(() => {
+  //   const subscription = AppState.addEventListener('change', nextAppState => {
+  //     const colorScheme = Appearance.getColorScheme();
+  //     setCurrentTheme( colorScheme === 'dark' ? 'dark' : 'light')
+
+  //   });
+
+  //   return () => {
+  //     subscription.remove();
+  //   };
+  // }, []);
+
+
+  
 
 
   const setTheme = (theme: ThemeColor) => {
